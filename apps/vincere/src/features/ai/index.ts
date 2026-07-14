950a95c0d764e9b49e2f7db9c3daff75613962d1
keimkevinkk-netizen <keim.kevin.kk@gmail.com>
2026-07-14 09:01:57 +0200

    feat(vincere): export sales coach modules

---
+++ apps/vincere/src/features/ai/index.ts
@@ -0,0 +1,5 @@
+export * from './salesCoachEngine';
+export * from './salesCoachKnowledge';
+export * from './salesCoachSchemas';
+export * from './salesCoachTypes';
+export * from './mockSalesCoachProvider';
