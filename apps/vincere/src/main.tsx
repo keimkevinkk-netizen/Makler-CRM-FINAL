7f3bc747632f343b2d54563e16a70422c5bcb43b
keimkevinkk-netizen <keim.kevin.kk@gmail.com>
2026-07-14 07:20:11 +0200

    feat(vincere): wire authentication into application bootstrap

--- apps/vincere/src/main.tsx
+++ apps/vincere/src/main.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 import { AppStoreProvider } from './app/AppStore';
+import { AuthGate } from './auth/AuthGate';
+import { AuthProvider } from './auth/AuthContext';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><AppStoreProvider><App /></AppStoreProvider></React.StrictMode>,
+  <React.StrictMode>
+    <AuthProvider>
+      <AppStoreProvider>
+        <AuthGate><App /></AuthGate>
+      </AppStoreProvider>
+    </AuthProvider>
+  </React.StrictMode>,
 );
